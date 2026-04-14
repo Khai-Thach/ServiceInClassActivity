b491a47917c5aa1360a94003722b097c8a94946f
Khai Thach <tuq43307@temple.edu>
2026-04-14 12:05:32 -0400

    commit in case laptop dies

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,42 @@
 package edu.temple.myapplication
-
+/*
+TODO
+Replicate all button functions as Menu action
+Make each item an Action Button (if there is room)
+*/
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_start -> {
+                TimerService().start(1000)
+                Toast.makeText(this, "Timer started", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.action_stop -> {
+                TimerService().pause()
+                Toast.makeText(this, "Timer stopped", Toast.LENGTH_SHORT).show()
+            }
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
-
-        }
+        return super.onOptionsItemSelected(item)
     }
 }
